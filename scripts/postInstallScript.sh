bb6b553e22bc8f3dcadd3d4d0344386556e5fc0f
Yanchen Liu <yliu28@bfd-dev00.sv.walmartlabs.com>
2016-07-06 14:57:33 -0700

    Fixed the problem of RPM build execution

--- scripts/postInstallScript.sh
+++ scripts/postInstallScript.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 
-for DEST in /usr/local/bin/dfstouch
+for DEST in /usr/local/bin/boo
 do
   if [[ -f $DEST && ! -L $DEST ]] ; then
     mv $DEST $DEST.old
   fi
 
-  # Install this version of dfstouch as one of alternative
+  # Install this version of boo as one of alternative
   alternatives --install $DEST `basename $DEST` $DEST.${rpmversion} `echo ${rpmversion}| sed 's/\.//g'`
 done
