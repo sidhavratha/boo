04cf1866f904b1330821ff9933dd94583f86afa8
Yanchen Liu <yliu@walmartlabs.com>
2016-11-10 15:30:03 -0800

    Remove catch printed info

--- src/main/java/com/wm/bfd/oo/workflow/BuildAllPlatforms.java
+++ src/main/java/com/wm/bfd/oo/workflow/BuildAllPlatforms.java
@@ -443,8 +443,6 @@ public class BuildAllPlatforms extends AbstractWorkflow {
       try {
         Thread.sleep(1);
       } catch (InterruptedException e) {
-        // TODO Auto-generated catch block
-        e.printStackTrace();
       }
     }
   }
