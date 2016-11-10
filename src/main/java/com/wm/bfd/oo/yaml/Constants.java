aa7fe159698427ba2ddb44367ae5d2497d6be0fd
Yanchen Liu <yliu@walmartlabs.com>
2016-11-10 09:54:45 -0800

    DTBFDTECH-585: Parallelize similar operations

--- src/main/java/com/wm/bfd/oo/yaml/Constants.java
+++ src/main/java/com/wm/bfd/oo/yaml/Constants.java
@@ -27,6 +27,7 @@ public class Constants {
   public static final String SIZE = "size";
   public static final String CINAME = "ciName";
   public static final String OFFLINE = "offline";
+  public static final String AUTHO_KEYS = "authorized_keys";
   // public static final String ASSEMBLY_PREFIX = "CUBEBOO-"; // Magic string.
   public static final String ASSEMBLY_NAME_TOO_LONG =
       "Assembly name cannot be more than 32 characters long. Please fix your assembly name in YAML.";
