8ee767ac10c88419c962e9b12d610a1a36962419
Mengmeng Liu <mengmeng.liu@walmartlabs.com>
2017-03-20 18:41:32 -0700

    Fix the format issue of the headers

--- src/test/java/com/oneops/boo/ITBuildAllPlatforms.java
+++ src/test/java/com/oneops/boo/ITBuildAllPlatforms.java
@@ -1,17 +1,15 @@
 /*
  * Copyright 2017 Walmart, Inc.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
  */
 package com.oneops.boo;
 
@@ -36,7 +34,8 @@ import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
 /**
- * Packages always change in OneOps, this test mainly focus on if functions can run without exceptions so far.
+ * Packages always change in OneOps, this test mainly focus on if functions can run without
+ * exceptions so far.
  *
  */
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
@@ -117,7 +116,7 @@ public class ITBuildAllPlatforms extends BooTest {
 
     System.out.println("Platform cloud scale update");
     assertTrue(build.updatePlatformCloudScale());
-    
+
     System.out.println("Platform component update");
     assertTrue(build.updatePlatformComponents());
 
